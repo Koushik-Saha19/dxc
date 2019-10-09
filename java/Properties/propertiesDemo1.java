1f63f4e9a70ca8dc60b28db93fb5a577a7aaabe3
Koushik-Saha19 <55374424+Koushik-Saha19@users.noreply.github.com>
2019-10-09 23:20:11 +0530

    Add files via upload

---
+++ java/Properties/propertiesDemo1.java
@@ -0,0 +1,23 @@
+package Properties;
+
+import java.util.Enumeration;
+import java.util.Properties;
+
+
+
+public class propertiesDemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Properties all = System.getProperties();
+		System.out.println(all);
+		
+		Enumeration <Object> elements = all.elements();
+		
+		while(elements.hasMoreElements()) {
+			System.out.println(elements.nextElement());
+		}
+	}
+
+}
